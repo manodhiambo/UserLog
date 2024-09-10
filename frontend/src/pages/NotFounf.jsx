755b13d22ada253f561a8a438ec17262a5cd1bc0
manodhiambo <manodhiambo@gmail.com>
2024-09-10 17:44:46 +0300

    Initial commits

---
+++ frontend/src/pages/NotFounf.jsx
@@ -0,0 +1,10 @@
+function NotFound()
+{
+
+	        return <div> 
+		<h1>404 Not Found</h1>
+		<p>The page you're looking for does'nt exist</p>
+		</div>
+}
+
+report default NotFound
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,7 @@
+function Register()
+{
+
+	        return <div>Register</div>
+}
+
+report default Register
